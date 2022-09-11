5eabe44cf08697f20ee080cdeea7d39007a44f1d
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2022-09-11 11:06:18 +0530

    inital Commit

---
+++ main.tf
@@ -0,0 +1,130 @@
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = var.region
+}
+
+resource "aws_vpc" "My-VPC" {
+    cidr_block = var.vpc_cidr
+    tags = {
+      "Name" = "My-VPC"
+    }
+}
+
+resource "aws_subnet" "PublicSubnet1" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1a"
+    tags = {
+      "Name" = "PublicSubnet1"
+    }
+}
+
+resource "aws_subnet" "PublicSubnet2" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1b"
+    tags = {
+      "Name" = "PublicSubnet2"
+    }
+}
+
+resource "aws_subnet" "PrivateSubnet1" {
+    cidr_block = "10.0.2.0/24"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1a" 
+    tags = {
+        Name = "PrivateSubnet1"
+    } 
+}
+
+resource "aws_subnet" "PrivateSubnet2" {
+    cidr_block = "10.0.3.0/24"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1b"
+    tags = {
+      "Name" = "PrivateSubnet2"
+    }
+}
+
+resource "aws_subnet" "PrivateSubnet3" {
+    cidr_block = "10.0.4.0/28"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1a" 
+    tags = {
+      "Name" = "PrivateSubnet3"
+    }
+}
+
+resource "aws_subnet" "PrivateSubnet4" {
+    cidr_block = "10.0.5.0/28"
+    vpc_id = aws_vpc.My-VPC.id
+    availability_zone = "us-east-1b"
+    tags = {
+      "Name" = "PrivateSubnet4"
+    }
+}
+
+resource "aws_internet_gateway" "IGW" {
+    vpc_id = aws_vpc.My-VPC.id 
+    tags = {
+        Name = "IGW"
+    } 
+}
+
+resource "aws_eip" "elastic-ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "NAT" {
+    subnet_id = aws_subnet.PublicSubnet1.id 
+    allocation_id = aws_eip.elastic-ip.id
+    depends_on = [
+      aws_internet_gateway.IGW
+    ] 
+}
+
+resource "aws_route_table" "WebRouteTable" {
+    vpc_id = aws_vpc.My-VPC.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.IGW.id
+    }
+  
+}
+
+resource "aws_route_table" "AppRouteTable" {
+    vpc_id = aws_vpc.My-VPC.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.NAT.id
+    }  
+}
+
+resource "aws_route_table" "DBRouteTable" {
+    vpc_id = aws_vpc.My-VPC.id  
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.NAT.id
+    }
+}
+
+
+
+
+
+
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,20 @@
+
+aws_access_key = "AKIAZNPDAFIEWX2L2L3H"
+
+aws_secret_key = "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"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
